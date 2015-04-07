2beb88221dc009922aaaebbfff811a958baa1298
Hanni Abukhater <hanniabu@gmail.com>
2015-04-07 03:19:46 -0400

    added calc site

---
+++ projects/calc/assets/js/switcheroo.js
@@ -0,0 +1,32 @@
+function disable() {
+	document.getElementById('default').style.display = "block";	
+	document.getElementById('paint').style.display = "none";
+	document.getElementById('drywall').style.display = "none";
+	document.getElementById('tile').style.display = "none";
+
+};
+
+window.onload = disable;
+
+function paint() {
+	document.getElementById('default').style.display = "none";
+	document.getElementById('paint').style.display = "block";
+	document.getElementById('drywall').style.display = "none";
+	document.getElementById('tile').style.display = "none";
+
+};
+
+function drywall() {
+	document.getElementById('default').style.display = "none";
+	document.getElementById('paint').style.display = "none";
+	document.getElementById('drywall').style.display = "block";
+	document.getElementById('tile').style.display = "none";
+
+};
+
+function tile() {
+	document.getElementById('default').style.display = "none";
+	document.getElementById('paint').style.display = "none";
+	document.getElementById('drywall').style.display = "none";
+	document.getElementById('tile').style.display = "block";
+};
