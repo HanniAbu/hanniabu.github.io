a934a6070b1c6d3d27dca0aecc6db8b51ff165bc
Hanni Abukhater <hanniabu@gmail.com>
2015-04-26 05:12:20 -0400

    initial push, first test

--- projects/portfolio/assets/js/main.js
+++ projects/portfolio/assets/js/main.js
@@ -9,7 +9,7 @@ $(function () {
 		var id = $(this).attr("href");
 		var offset = 0;
 		var target = $(id).offset().top - offset;
-		$('html, body').animate({scrollTop:target}, 800);
+		$('body').animate({scrollTop:target}, 800);
 		event.preventDefault();
 	});
 });
