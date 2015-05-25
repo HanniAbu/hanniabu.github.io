9babc84362d833ba18871f0c9bffc5123b444337
Hanni Abukhater <hanniabu@gmail.com>
2015-05-25 04:07:33 -0400

    portfolio initial commit and file restructuring

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+// function test() {
+// 	document.getElementById('bottom-view').style.display = 'none';
+// }
+
+
+// eases the scrolling to page sections
+$(function () {
+	$('a[href^="#"]').click(function(event) {
+		var id = $(this).attr("href");
+		var offset = 0;
+		var target = $(id).offset().top - offset;
+		$('body').animate({scrollTop:target}, 800);
+		event.preventDefault();
+	});
+});
