6c2da92b85e6be0437604adb189ef20c30833609
Hanni Abukhater <hanniabu@gmail.com>
2015-05-28 21:42:08 -0400

    edit easing to stop stutter scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,16 @@
-// function test() {
-// 	document.getElementById('bottom-view').style.display = 'none';
-// }
-
 
 // eases the scrolling to page sections
-$(function () {
-	$('a[href^="#"]').click(function(event) {
-		var id = $(this).attr("href");
-		var offset = 0;
-		var target = $(id).offset().top - offset;
-		$('body').animate({scrollTop:target}, 800);
-		event.preventDefault();
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
 	});
 });
